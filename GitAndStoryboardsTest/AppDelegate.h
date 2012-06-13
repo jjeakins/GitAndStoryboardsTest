ee13473a91849886b4458d3aa2b5c50b94106d30
Jeff Jeakins <jjeakins@gmail.com>
2012-06-13 10:48:35 -0400

    Initial Commit

---
+++ GitAndStoryboardsTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitAndStoryboardsTest
+//
+//  Created by Jeff Jeakins on 12-06-13.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitAndStoryboardsTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitAndStoryboardsTest
+//
+//  Created by Jeff Jeakins on 12-06-13.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
